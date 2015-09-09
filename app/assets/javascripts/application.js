f0012cd54ab80f1ddc2915c7a6d433d287c43336
varsha-bharath <varsha.jp3@gmail.com>
2015-09-09 12:39:02 +0530

    9.9.2015

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,6 +9,8 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-////= require turbolinks
-//= require_tree .
+//
 //= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
